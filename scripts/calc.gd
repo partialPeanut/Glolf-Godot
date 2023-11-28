ad5ec81543073b642a8f2fe904dd732a3b5a3f82
partialPeanut <penny@scharftech.com>
2023-11-28 07:32:13 -0700

    Update calc.gd

--- scripts/calc.gd
+++ scripts/calc.gd
@@ -1,2 +1,8 @@
 extends Node
 class_name Calc
+
+# 0. Every point on the field has a "Pogness" value based on terrain and distance to hole
+# 1. Draw circles based on estimated distances of different strokes and yeetness
+# 2. Use smartassery to determine stroke type
+# 3. Use topology to decide where to aim
+# 4. Use trigonometry (consistency?) to determine variance from target point
